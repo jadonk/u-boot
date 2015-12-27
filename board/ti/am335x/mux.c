ffc19ba7c23b72953283abb1f7dd086017a8f6a0
Jason Kridner <jdk@ti.com>
2015-12-27 22:01:38 +0000

    Force OSD3358

--- board/ti/am335x/mux.c
+++ board/ti/am335x/mux.c
@@ -365,7 +365,10 @@ void enable_board_pin_mux(struct am335x_baseboard_id *header)
 		configure_module_pin_mux(mmc1_pin_mux);
 #endif
 	} else {
-		puts("Unknown board, cannot configure pinmux.");
-		hang();
+		puts("Unknown board: assuming OSD3358.");
+		configure_module_pin_mux(i2c1_pin_mux);
+		configure_module_pin_mux(mii1_pin_mux);
+		configure_module_pin_mux(mmc0_pin_mux);
+		configure_module_pin_mux(mmc1_pin_mux);
 	}
 }
